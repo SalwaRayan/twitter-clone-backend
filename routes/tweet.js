19be14557e1c26d87a7a524a825d712b46aeef9b
daoraCode <severin.mboukou@gmail.com>
2022-01-15 19:12:26 +0100

    delete tweet done

--- routes/tweet.js
+++ routes/tweet.js
@@ -6,9 +6,9 @@ const app = express()
 
 // post a new tweet
 app.post('/:id/tweet', async (req, res) => {
+
+    
     const { id } = req.params
-    // current tweet user
-    const { user } = req.body
 
     try {
         // creation of the new tweet (instanciation)
@@ -17,9 +17,13 @@ app.post('/:id/tweet', async (req, res) => {
         // register the new tweet made by user
         tweet.save(async (err, tweet) => {
             if (tweet) {
+
                 // each tweet is created so linked by a unique user
                 // so it is necessary to create a relation between the new tweet created
                 // by user that already exists
+
+                // 
+                tweet.user = id
                 const getCurrentUser = await User.findOne({ _id: id })
                 
                 // console.log(getCurrentUser)
@@ -39,8 +43,23 @@ app.post('/:id/tweet', async (req, res) => {
     }
 })
 
-app.delete('/:id/tweet', (req, res) => {
-    // console.log();
+// route created to delete a tweet
+app.delete('/:id/tweet', async (req, res) => {
+
+    // get the tweet id in order for target it first
+    const { id } = req.params
+
+    try {
+        // delete the current tweet once its id retrieved
+        await Tweet.findOneAndDelete({ _id: id }).exec()
+        // find the current user who posted the tweet with an id and remove the id of the tweet from the tweets Array of User
+        await User.findOneAndUpdate({ tweets: id }, { $pull: { tweets: id }})
+
+        res.json({ success: 'Tweet deleted' })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ error: err })
+    }
 })
 
 module.exports = app
